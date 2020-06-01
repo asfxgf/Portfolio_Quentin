3feb01c3d150a54fd4ace918d1287127eea76779
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-06-01 16:50:38 +0200

    ajout des routes de la home, tout est fonctionnel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,27 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :convictions]
+  skip_before_action :authenticate_user!, only: [:home, :convictions, :competences, :ressources, :projets, :bibliotheque, :articles, :videos]
 
   def home
   end
 
   def convictions
+  end
+
+  def competences
+  end
+
+  def ressources
+  end
+
+  def projets
+  end
+
+  def bibliotheque
+  end
+
+  def articles
+  end
+
+  def videos
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get "convictions", to: "pages#convictions"
+  get "competences", to: "pages#competences"
+  get "ressources", to: "pages#ressources"
+  get "projets", to: "pages#projets"
+  get "bibliotheque", to: "pages#bibliotheque"
+  get "articles", to: "pages#articles"
+  get "videos", to: "pages#videos"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
