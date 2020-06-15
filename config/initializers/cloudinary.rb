f9a044ba334ef244e358e0329f9987b2c32c392c
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-06-15 22:46:01 +0200

    add cloudinary in gems and initializers

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'asfxgf'
+  config.api_key = '728696211196552'
+  config.api_secret = '5b6TFog5A9rGjX3VRUff4AtJ-e8'
+  config.secure = true
+  config.cdn_subdomain = true
+end
