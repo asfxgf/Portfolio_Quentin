0804d4c4a6d38630b4f9060b2ada1c936fd4209c
quentin deleglise <deleglise.quentin@hotmail.fr>
2020-06-24 18:47:35 +0200

    annulation de auth 1 pour auth 2

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
-end
